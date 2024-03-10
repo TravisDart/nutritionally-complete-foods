96d8553823417d4a0900604a17411759cf302af4
Travis Dart <travisdart@travisdart.com>
2024-03-10 00:17:56 -0500

    Benchmarking code

--- solve/find_coefficients_for_solution.py
+++ solve/find_coefficients_for_solution.py
@@ -25,7 +25,7 @@ solve the vector x to find the quantities of food.
 
 import numpy as np
 from scipy.optimize import minimize
-
+import time
 from load_data import load_requirements
 
 
@@ -67,6 +67,165 @@ def evaluate_result(solution, min_bound, max_bound):
     return error, under_bounds, over_bounds, is_out_of_bounds
 
 
+def benchmark_solving():
+    # TODO: un-duplicate this code
+    example_foods = [
+        [
+            "14091",
+            "Beverages",
+            "Beverages, almond milk, unsweetened, shelf stable",
+            "",
+            (1840, "mg"),
+            (13, "g"),
+            (31, "mg"),
+            (0, "mg"),
+            (150, "kcal"),
+            (10, "g"),
+            (2, "g"),
+            (0, None),
+            (10, "µg"),
+            (3, "mg"),
+            (60, "mg"),
+            (400, "µg"),
+            (1, "mg"),
+            (90, "mg"),
+            (670, "mg"),
+            (4, "g"),
+            (0, "mg"),
+            (1, "µg"),
+            (720, "mg"),
+            (0, "mg"),
+            (0, "µg"),
+            (0, "mg"),
+            (0, "µg"),
+            (0, "mg"),
+            (10, "µg"),
+            (63, "mg"),
+            (0, "µg"),
+            (965, "g"),
+            (1, "mg"),
+        ],
+        [
+            "14355",
+            "Beverages",
+            "Beverages, tea, black, brewed, prepared with tap water",
+            "",
+            (0, "mg"),
+            (3, "g"),
+            (4, "mg"),
+            (0, "mg"),
+            (10, "kcal"),
+            (0, "g"),
+            (0, "g"),
+            (3730, "µg"),
+            (50, "µg"),
+            (0, "mg"),
+            (30, "mg"),
+            (2190, "µg"),
+            (0, "mg"),
+            (10, "mg"),
+            (370, "mg"),
+            (0, "g"),
+            (0, "mg"),
+            (0, "µg"),
+            (30, "mg"),
+            (0, "mg"),
+            (0, "µg"),
+            (0, "mg"),
+            (0, "µg"),
+            (0, "mg"),
+            (0, "µg"),
+            (0, "mg"),
+            (0, "µg"),
+            (997, "g"),
+            (0, "mg"),
+        ],
+        [
+            "9024",
+            "Fruits and Fruit Juices",
+            "Apricots, canned, juice pack, with skin, solids and liquids",
+            "",
+            (120, "mg"),
+            (123, "g"),
+            (18, "mg"),
+            (1, "mg"),
+            (480, "kcal"),
+            (0, "g"),
+            (16, "g"),
+            (0, None),
+            (20, "µg"),
+            (3, "mg"),
+            (100, "mg"),
+            (520, "µg"),
+            (3, "mg"),
+            (200, "mg"),
+            (1650, "mg"),
+            (6, "g"),
+            (0, "mg"),
+            (1, "µg"),
+            (40, "mg"),
+            (0, "mg"),
+            (850, "µg"),
+            (1, "mg"),
+            (0, "µg"),
+            (49, "mg"),
+            (0, "µg"),
+            (6, "mg"),
+            (22, "µg"),
+            (866, "g"),
+            (1, "mg"),
+        ],
+        [
+            "11672",
+            "Vegetables and Vegetable Products",
+            "Potato pancakes",
+            "",
+            (320, "mg"),
+            (278, "g"),
+            (742, "mg"),
+            (2, "mg"),
+            (2677, "kcal"),
+            (148, "g"),
+            (33, "g"),
+            (4, "µg"),
+            (410, "µg"),
+            (17, "mg"),
+            (360, "mg"),
+            (2600, "µg"),
+            (17, "mg"),
+            (1280, "mg"),
+            (6220, "mg"),
+            (61, "g"),
+            (2, "mg"),
+            (89, "µg"),
+            (7640, "mg"),
+            (2, "mg"),
+            (320, "µg"),
+            (4, "mg"),
+            (3, "µg"),
+            (276, "mg"),
+            (3, "µg"),
+            (2, "mg"),
+            (27, "µg"),
+            (478, "g"),
+            (7, "mg"),
+        ],
+    ]
+    just_matrix_coefficients = [[y[0] for y in x[4:]] for x in example_foods]
+    A = np.array(just_matrix_coefficients)
+    A = A.T
+    min_requirements, max_requirements, _ = load_requirements()
+
+    # About 100 solutions per second
+    t = time.process_time()
+    iterations = 1000
+    for x in range(iterations):
+        find_closest_solution(A, min_requirements)
+    elapsed_time = time.process_time() - t
+    print("elapsed_time", elapsed_time, "iter/sec:", iterations / elapsed_time)
+    return
+
+
 def real_test():
     # Test 2: Square A
     # fmt: off
@@ -85,7 +244,10 @@ def real_test():
     ]
     just_matrix_coefficients = [[y[0] for y in x[4:]] for x in example_foods]
     A = np.array(just_matrix_coefficients)
+    A = A.T
     min_requirements, max_requirements, _ = load_requirements()
+
+
 
     example_solution = np.array([[2759, 1199, 804, 1006]])
     print(example_solution.shape)
