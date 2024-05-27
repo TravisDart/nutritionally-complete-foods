01a4112fd427fd780dd12f884b1daca9459a44da
Travis Dart <travisdart@travisdart.com>
2024-05-26 21:53:55 -0400

    Removed non-ortools solvers, as they were unreliable.

--- solve/find_coefficients_for_solution.py
+++ solve/find_coefficients_for_solution.py
@@ -31,75 +31,7 @@ from solve import solve_it
 from constants import KNOWN_SOLUTIONS
 
 
-def find_closest_solution3(A, c, d):
-    # def objective_function(x):
-    #     """Objective function to minimize the sum of squared slack variables."""
-    #     lower_slack = np.maximum(0, c - A @ x)  # Non-negative slack for lower bound
-    #     upper_slack = np.maximum(0, A @ x - d)  # Non-negative slack for upper bound
-    #     return np.sum(lower_slack**2 + upper_slack**2)
-
-    def objective_function(x):
-        """Objective function to minimize the sum of squared slack variables."""
-        slack = np.maximum(0, c - A @ x)  # Non-negative slack variables
-        return np.sum(slack**2)
-
-    # Initial guess for x (can be adjusted if needed)
-    x0 = np.ones(4)
-    # x0 = np.array([1227, 804, 3382, 913])
-
-    # Define constraints to ensure A*x > c element-wise
-    constraints = [
-        {"type": "ineq", "fun": lambda x: A @ x - c},
-        {"type": "ineq", "fun": lambda x: d - A @ x},
-    ]
-
-    # TODO: try L-BFGS-B or Nelder-Mead.
-    # Minimize the objective function subject to the constraints
-    res = minimize(objective_function, x0, method="SLSQP", constraints=constraints)
-
-    # Check if a solution is found and strictly satisfies the constraints
-    print("res.success", res.success)
-    if res.success:
-        print("res.x", res.x)
-        solution = res.x
-        if np.all(A @ solution > c) and np.all(A @ solution < d):  # Strict satisfaction
-            return solution
-        else:
-            return None  # No solution that strictly satisfies the inequality
-    else:
-        return None  # Optimization failed to find a solution
-
-
-def find_closest_solution2(A, c):
-    x = np.linalg.lstsq(A, c, rcond=None)
-    x = np.ceil(x[0])
-    return x
-
-
-def find_closest_solution(A, c):
-    """
-    Finds a solution x to the equation c < A*x, minimizing distance to c.
-
-    In our application:
-      c is the minimum requirements for each nutrient
-      A is the matrix of nutrients in each food
-    """
-
-    def objective_function(x, A, c):
-        """Calculates the squared distance between c and A*x."""
-        return np.linalg.norm(A @ x - c)  # ** 2
-
-    minimize_result = minimize(
-        objective_function,
-        x0=np.zeros(A.shape[1]),
-        args=(A, c),
-        bounds=[(0, np.inf) for _ in range(A.shape[1])],
-    )
-    result = np.ceil(minimize_result.x)
-    return result
-
-
-def evaluate_result(solution, min_bound, max_bound, verbose=True, assert_good=False):
+def _evaluate_result(solution, min_bound, max_bound, verbose=True, assert_good=False):
     error = np.linalg.norm(solution - min_bound)
     under_bounds = [solution[x] < min_bound[x] for x in range(len(max_bound))]
     under_bounds_str = "".join(["1" if x else "0" for x in under_bounds])
@@ -145,7 +77,7 @@ def benchmark_solving():
     t = time.process_time()
     iterations = 1000
     for x in range(iterations):
-        find_closest_solution(A, min_requirements)
+        solve_it(min_requirements, max_requirements, example_foods)
     elapsed_time = time.process_time() - t
     print("elapsed_time", elapsed_time, "iter/sec:", iterations / elapsed_time)
 
@@ -163,7 +95,7 @@ def multiply_known_solutions():
 
         solution = [x[1] for x in known_solution]
         result = A @ solution
-        evaluate_result(result, min_requirements, max_requirements, assert_good=False)
+        _evaluate_result(result, min_requirements, max_requirements, assert_good=False)
 
 
 def solve_against_known_solutions():
@@ -194,7 +126,7 @@ def solve_against_known_solutions():
         # solution = list([x[1] for x in sorted_tuples])
 
         result = A @ solution
-        evaluate_result(result, min_requirements, max_requirements)
+        _evaluate_result(result, min_requirements, max_requirements)
 
 
 def tests2():
