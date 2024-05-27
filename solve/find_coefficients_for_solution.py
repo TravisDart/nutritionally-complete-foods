b6ab47c7e384d8b19b71ee258d41a5e31a60eca5
Travis Dart <travisdart@travisdart.com>
2024-05-26 22:49:21 -0400

    Finished tests

--- solve/find_coefficients_for_solution.py
+++ solve/find_coefficients_for_solution.py
@@ -24,11 +24,15 @@ solve the vector x to find the quantities of food.
 """
 
 import numpy as np
-from scipy.optimize import minimize
 import time
 from load_data import load_requirements, load_subset_of_data, load_test_data
 from solve import solve_it
 from constants import KNOWN_SOLUTIONS
+
+
+def ordered_dict_values(food_quantity: dict[str, int]):
+    """Turn the dictionary into a list of values, being sure to preserve the id order of the keys."""
+    return [x[1] for x in sorted(food_quantity.items(), key=lambda item: int(item[0]))]
 
 
 def _evaluate_result(solution, min_bound, max_bound, verbose=True, assert_good=False):
@@ -59,29 +63,6 @@ def _evaluate_result(solution, min_bound, max_bound, verbose=True, assert_good=F
     )
 
 
-def benchmark_solving():
-    example_foods = load_subset_of_data(
-        ids=[
-            14091,
-            14355,
-            9068,
-            11672,
-        ]
-    )
-    just_matrix_coefficients = [[y[0] for y in x[4:]] for x in example_foods]
-    A = np.array(just_matrix_coefficients)
-    A = A.T
-    min_requirements, max_requirements, _ = load_requirements()
-
-    # About 100 solutions per second
-    t = time.process_time()
-    iterations = 1000
-    for x in range(iterations):
-        solve_it(min_requirements, max_requirements, example_foods)
-    elapsed_time = time.process_time() - t
-    print("elapsed_time", elapsed_time, "iter/sec:", iterations / elapsed_time)
-
-
 def multiply_known_solutions():
     """Multiply out each known solution to see if it's really a solution."""
 
@@ -95,10 +76,10 @@ def multiply_known_solutions():
 
         solution = [x[1] for x in known_solution]
         result = A @ solution
-        _evaluate_result(result, min_requirements, max_requirements, assert_good=False)
+        _evaluate_result(result, min_requirements, max_requirements, assert_good=True)
 
 
-def solve_against_known_solutions():
+def solve_against_known_solutions(verbose=True):
     """Run the solver against just the foods in the known solutions to verify that a solution can be found."""
 
     for known_solution in KNOWN_SOLUTIONS[4]:
@@ -111,112 +92,76 @@ def solve_against_known_solutions():
 
         # Part 2: Solve it with the solver
         solver_result = solve_it(
-            min_requirements, max_requirements, example_foods, log_level=1
+            min_requirements, max_requirements, example_foods, log_level=int(verbose)
         )
         assert len(solver_result) == 1  # Only one solution
 
         food_quantity = list(solver_result.values())[0]["food_quantity"]
-        solution = []
-        # TODO: There's probably a better order-preserving way to do this.
-        for id in ids:
-            solution += [food_quantity[str(id)]]
-
-        # # Also works
-        # food_quantity = list(solver_result.values())[0]["food_quantity"]
-        # sorted_tuples = sorted(food_quantity.items(), key=lambda item: int(item[0]))
-        # solution = list([x[1] for x in sorted_tuples])
+        solution = ordered_dict_values(food_quantity)
 
         result = A @ solution
-        _evaluate_result(result, min_requirements, max_requirements)
+        _evaluate_result(
+            result,
+            min_requirements,
+            max_requirements,
+            verbose=verbose,
+            assert_good=True,
+        )
 
 
-def tests2():
-    nutrients, example_foods, min_requirements, max_requirements = load_test_data()
-    solver_result = solve_it(
-        min_requirements,
-        max_requirements,
-        example_foods,
-        num_foods=len(example_foods),
-        log_level=1,
+def trivial_tests(verbose=True):
+    for food_set in range(3):
+        nutrients, example_foods, min_requirements, max_requirements = load_test_data(
+            food_set
+        )
+        solver_result = solve_it(
+            min_requirements,
+            max_requirements,
+            example_foods,
+            num_foods=len(example_foods),
+            log_level=int(verbose),
+        )
+
+        just_matrix_coefficients = [[y[0] for y in x[4:]] for x in example_foods]
+        A = np.array(just_matrix_coefficients)
+        A = A.T
+
+        food_quantity = list(solver_result.values())[0]["food_quantity"]
+        solution = ordered_dict_values(food_quantity)
+
+        result = A @ solution
+        _evaluate_result(
+            result,
+            min_requirements,
+            max_requirements,
+            verbose=verbose,
+            assert_good=True,
+        )
+        if verbose:
+            print(f"Test {food_set} passed")
+
+
+def benchmark_solving():
+    print("Benchmarking solver speed")
+    # About 100 solutions per second
+    t = time.process_time()
+    iterations = 100
+    for x in range(iterations):
+        solve_against_known_solutions(verbose=False)
+        trivial_tests(verbose=False)
+    elapsed_time = time.process_time() - t
+    print(
+        "elapsed_time",
+        elapsed_time,
+        "solutions/sec:",
+        iterations * (len(KNOWN_SOLUTIONS) + 3) / elapsed_time,
     )
-
-    just_matrix_coefficients = [[y[0] for y in x[4:]] for x in example_foods]
-    A = np.array(just_matrix_coefficients)
-    A = A.T
-
-    food_quantity = list(solver_result.values())[0]["food_quantity"]
-    solution = []
-    # TODO: There's probably a better order-preserving way to do this.
-    for id in [1, 2, 3]:
-        solution += [food_quantity[str(id)]]
-
-    result = A @ solution
-    _evaluate_result(result, min_requirements, max_requirements)
-    print("Test2 passed")
-
-
-def tests():
-    # Test 1: A is non-square
-    # fmt: off
-    A = np.array([
-        [1, 0, 0],
-        [0, 1, 1]
-    ])  # Non-square A
-    # fmt: on
-    A = A.T
-    c = np.array([2, 2, 2])
-
-    x = find_closest_solution(A, c)
-    assert np.array_equal(x, [2, 2])
-    print("Test1 passed")
-
-    # Test 2: Square A
-    # fmt: off
-    A = np.array([
-        [1, 0, 0],
-        [0, 1, 0],
-        [0, 0, 1],
-    ]) 
-    # fmt: on
-    A = A.T
-    c = np.array([2, 2, 2])
-
-    x = find_closest_solution(A, c)
-    assert np.array_equal(x, [2, 2, 2])
-    print("Test2 passed")
-
-    # Test 3: Floats
-    # fmt: off
-    A = np.array([
-        [1, .5, 0],
-        [0, .5, 1],
-    ])
-    # fmt: on
-    A = A.T
-    c = np.array([2, 2, 2])
-
-    x = find_closest_solution(A, c)
-    assert np.array_equal(x, [2, 2])
-    print("Test3 passed")
-
-    # Test 4: 1-dimensional
-    # fmt: off
-    A = np.array([
-        [1, 1, 1],
-    ])
-    # fmt: on
-    A = A.T
-    c = np.array([2, 2, 2])
-
-    x = find_closest_solution(A, c)
-    assert np.array_equal(
-        x,
-        [2, 2],
-    )
-    print("Test4 passed")
 
 
 if __name__ == "__main__":
-    tests2()
-    # multiply_known_solutions()
-    # solve_against_known_solutions()
+    trivial_tests()
+    multiply_known_solutions()
+    solve_against_known_solutions()
+    print("All assertions pass.")
+
+    # benchmark_solving()
--- solve/load_data.py
+++ solve/load_data.py
@@ -3,7 +3,7 @@ import random
 from constants import NUMBER_SCALE, MAX_NUMBER
 
 
-def load_test_data():
+def load_test_data(food_set: int = 0):
     # This is similar to the format of the CSV data.
     nutrients = [
         ["Vitamin A", (1, "mg"), (10, "mg")],
@@ -11,12 +11,23 @@ def load_test_data():
         ["Vitamin C", (1, "mg"), (10, "mg")],
     ]
 
-    # Each nutritional value for each food
-    foods = [  #                           Vitamins A           B          C
-        [1, "Food A", "Food", "Scientific_Name", (1, "mg"), (0, "mg"), (0, "mg")],
-        [2, "Food B", "Food", "Scientific_Name", (0, "mg"), (1, "mg"), (0, "mg")],
-        [3, "Food C", "Food", "Scientific_Name", (0, "mg"), (0, "mg"), (1, "mg")],
+    food_sets = [
+        [  # Square                             Vitamins  A          B          C
+            [1, "Food A", "Food", "Scientific_Name", (1, "mg"), (0, "mg"), (0, "mg")],
+            [2, "Food B", "Food", "Scientific_Name", (0, "mg"), (1, "mg"), (0, "mg")],
+            [3, "Food C", "Food", "Scientific_Name", (0, "mg"), (0, "mg"), (1, "mg")],
+        ],
+        [  # Non-square                        Vitamins  A          B          C
+            [1, "Food A", "Food", "Scientific_Name", (1, "mg"), (0, "mg"), (0, "mg")],
+            [2, "Food B", "Food", "Scientific_Name", (0, "mg"), (1, "mg"), (1, "mg")],
+        ],
+        [  # Sort-of simulate floats            Vitamins  A           B          C
+            [1, "Food A", "Food", "Scientific_Name", (10, "mg"), (5, "mg"), (0, "mg")],
+            [2, "Food B", "Food", "Scientific_Name", (0, "mg"), (5, "mg"), (10, "mg")],
+        ],
     ]
+
+    foods = food_sets[food_set]
 
     # This is how the data is parsed and used.
     min_requirements = [n[1][0] for n in nutrients]
