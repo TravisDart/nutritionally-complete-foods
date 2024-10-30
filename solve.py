370477ca6b10835043151f03d3d8d856f0f648a9
Travis Dart <travis.dart@connectwise.com>
2024-10-29 22:52:55 -0400

    Working on 1-solutions solver.

--- solve.py
+++ solve.py
@@ -12,58 +12,21 @@ from solver.utils import get_arg_parser
 
 
 class VarArraySolutionPrinter(cp_model.CpSolverSolutionCallback):
-    def __init__(
-        self,
-        variables,
-        min_requirements,
-        max_requirements,
-        foods,
-        error_for_quantity,
-        log_level: int = 0,
-    ):
+    def __init__(self, variables, error_for_quantity):
         super().__init__()
         self.__variables = variables
-        self.__foods = foods
-        self.__min_requirements = min_requirements
-        self.__max_requirements = max_requirements
         self.__error_for_quantity = error_for_quantity
+        self.__solution = None
 
-        self.__solutions = {}
-        self.__log_level = log_level
-
-    def get_solutions(self):
-        return self.__solutions
+    def get_solution(self):
+        return self.__solution
 
     def on_solution_callback(self):
         # Just the ordered IDs of the foods in the solution.
-        solution_id = tuple(
+        self.__solution = tuple(
             sorted([int(v.Name()) for v in self.__variables if self.Value(v) != 0])
         )
-
-        # Info on the solution.
-        solution_value = {
-            "food_quantity": {
-                v.Name(): self.Value(v) for v in self.__variables if self.Value(v) != 0
-            },
-            "avg_error": mean([self.Value(v) for v in self.__error_for_quantity]),
-            "total_error": sum([self.Value(v) for v in self.__error_for_quantity]),
-        }
-
-        # If we already have this combination of foods, pick the one with the lowest error.
-        if solution_id in self.__solutions:
-            if (
-                solution_value["total_error"]
-                < self.__solutions[solution_id]["total_error"]
-            ):
-                if self.__log_level >= 1:
-                    print("Found more optimal solution for", solution_id)
-                    print("Old solution:", self.__solutions[solution_id])
-                    print("New solution:", solution_value)
-                self.__solutions[solution_id] = solution_value
-        else:
-            self.__solutions[solution_id] = solution_value
-            if self.__log_level >= 1:
-                print("Found a new solution:", solution_id)
+        self.StopSearch()
 
 
 def print_info(status, solver, solution_printer):
@@ -73,10 +36,7 @@ def print_info(status, solver, solution_printer):
         print(f"  conflicts: {solver.NumConflicts()}")
         print(f"  branches : {solver.NumBranches()}")
         print(f"  wall time: {solver.WallTime()} s")
-        print(f"  sol found: {len(solution_printer.get_solutions())}")
-    # I'm pretty sure this used to work...
-    # elif status == cp_model.MODEL_INVALID:
-    #     print(cp_model.Validate())
+        print(f"  sol found: {solution_printer.get_solution()}")
     else:
         outcomes = [
             "UNKNOWN",
@@ -149,25 +109,17 @@ def solve_it(
     solver = cp_model.CpSolver()
     solver.parameters.log_search_progress = bool(log_level >= 2)
     solver.parameters.enumerate_all_solutions = True
-    # The solution printer displays the nutrient that is out of bounds.
-    solution_printer = VarArraySolutionPrinter(
-        intermediate_values,
-        min_requirements,
-        max_requirements,
-        foods,
-        error_for_quantity,
-        log_level=log_level,
-    )
+    solution_printer = VarArraySolutionPrinter(intermediate_values, error_for_quantity)
 
     status = solver.Solve(model, solution_printer)
     if log_level >= 1:
         print_info(status, solver, solution_printer)
 
     if status in [cp_model.OPTIMAL, cp_model.FEASIBLE]:
-        solutions = solution_printer.get_solutions()
+        solution = solution_printer.get_solution()
         if log_level:
-            pprint(solutions)
-        return solutions
+            print(solution)
+        return solution
 
 
 if __name__ == "__main__":
